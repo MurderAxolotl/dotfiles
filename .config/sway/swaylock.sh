05dbeec6630c654bde9ad74bc594dd73ffc5b116
SleepyStatic <49412250+psychon-night@users.noreply.github.com>
2025-09-20 16:53:17 -0700

    Sway config ehe

---
+++ .config/sway/swaylock.sh
@@ -0,0 +1,14 @@
+swaylock --screenshots --clock --indicator-idle-visible \
+	--effect-greyscale \
+	--indicator-radius 100 \
+	--indicator-thickness 7 \
+	--ring-color 455a64 \
+	--key-hl-color 3a2f4f \
+	--text-color 3e454f \
+	--line-color 00000000 \
+	--inside-color 00000088 \
+	--separator-color 00000000 \
+	--fade-in 1 \
+	--effect-scale 0.5 --effect-blur 7x3 --effect-scale 2 \
+	--effect-vignette 0.5:0.5 \
+	"$@"
